3aa4ecdc357ad31a8d465f2057a0f55dedeb9540
Abishek Khanal <90846785+Abishekkhanal@users.noreply.github.com>
2022-03-07 20:14:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,130 @@
+/*var x=10,y=11;
+x++;
+y--;
+y=(--y) + (x++) - 10 + (--x); 
+console.log("the value of x is "+ x);
+console.log("the value of y is "+y);
+
+var  a=-3, b=-7; 
+--a;  b--;
+a=(--b) - (++a) + 5 - (--b); 
+console.log("the value of a is "+ a);
+console.log("the value of b is "+b);  */
+
+
+ /*program to check if the number is even or odd
+// take input from the user
+const number = 10;
+
+//check if the number is even
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}*/
+
+
+//program to show month name according to number
+
+/*var weekNum=1;
+if(weekNum==1){
+    console.log("Monday");
+}
+else if (weekNum==2){
+    console.log("Tuesday");
+}
+else if (weekNum==3){
+    console.log("Wedsday");
+}
+else if (weekNum==4){
+    console.log("Thursday");
+}
+else if (weekNum==5){
+    console.log("Friday");
+}
+else if (weekNum==6){
+    console.log("Saturday");
+}
+else if (weekNum==7){
+    console.log("Sunday");
+}*/
+
+//Students Marks Grading System
+
+// var grade=1.5;
+// if (grade>=80 && grade <= 100){
+//     console.log("Your Marks is : " + grade +" and Grade is : "+"Grade A");
+// }
+// else if(grade>=70 && grade < 79){
+//     console.log("Your Marks is : " + grade +" and Grade is : "+"Grade B");
+// }
+// else if(grade>=60 && grade < 69){
+//     console.log("Your Marks is : " + grade +" and Grade is : "+"Grade C");
+// }
+// else if(grade>=40 && grade < 59){
+//     console.log("Your Marks is : " + grade +" and Grade is : "+"Grade D");
+// }
+// else if(grade<=39){
+//     console.log("Your Marks is : " + grade +" and Grade is : "+"Grade E");
+// }
+// else{
+//     console.log("Please Enter The Valid Marks..!")
+// }
+
+//Program to show months name by numbers..
+
+// function grading(){
+
+// var month= prompt("Enter any Month Number");
+// if(month==1){
+//  document.write("Month Number "+month +" is January");
+// }
+// else if(month==2){
+//     document.write("Month Number "+month +" is Fabruary");
+// }
+// else if(month==3){
+//     document.write("Month Number "+month +" is March");
+// }
+// else if(month==4){
+//     document.write("Month Number "+month +" is April");
+// }
+// else if(month==5){
+//     document.write("Month Number "+month +" is May");
+// }
+// else if(month==6){
+//     document.write("Month Number "+month +" is June");
+// }
+// else if(month==7){
+//     document.write("Month Number "+month +" is July");
+// }
+// else if(month==8){
+//     document.write("Month Number "+month +" is August");
+// }
+// else if(month==9){
+//     document.write("Month Number "+month +" is September");
+// }
+// else if(month==10){
+//     document.write("Month Number "+month +" is October");
+// }
+// else if(month==11){
+//     document.write("Month Number "+month +" is November");
+// }
+// else if(month==12){
+//     document.write("Month Number "+month +" is December");
+// }
+// else{
+//     document.write("Enter Valid Month Number..!")
+//     alert("Enter Valid Month Number..!")
+// }
+// }
+
+// code to find cube
+
+function cube(){
+   var a=  prompt("Enter a number");
+   var c= document.write("The cube of "+a+ " is "+ a*a);
+}
+
